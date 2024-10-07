68929370d154a160a4ad9efc87090a74562df6bc
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2024-10-07 13:31:17 +0200

    profile page content and module.css

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,15 +1,59 @@
 import profilePic from './../../assets/pictures/ProfilePic.png';
+import backArrow from './../../assets/icons/BackArrow.svg';
+import editBtn from './../../assets/icons/EditBtn.svg';
 import classes from './Profile.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default function ProfilePage() {
   return (
     <>
-      <div className={classes.profile}>
-        <h2>Profile</h2>
-        <img src={profilePic} alt="Profile picture" />
-        <h1>Sarah</h1>
-        <h1>Wegan</h1>
-      </div>
+      <header className={classes.profileHeader}>
+        <nav className={classes.profileNav}>
+          <ul className={classes.back}>
+            <li>
+              <NavLink to="/">
+                <img src={backArrow} alt="Sign for back" />
+              </NavLink>
+            </li>
+          </ul>
+          <h2>Profile</h2>
+          <ul className={classes.edit}>
+            <li>
+              <NavLink to="">
+                <img src={editBtn} alt="Button for editing profile" />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className={classes.profileInfo}>
+        <img src={profilePic} alt="Profile Picture" />
+        <h3>
+          <span>Sarah</span> Wegan
+        </h3>
+        <p>Basic Member</p>
+      </section>
+
+      <section className={classes.profileDetails}>
+        <div className={classes.measures}>
+          <p>
+            <span>53 kg</span> Wight
+          </p>
+          <p>
+            <span>160 cm</span> Height
+          </p>
+          <p>
+            <span>33 years</span> Age
+          </p>
+        </div>
+
+        <div>
+          <NavLink to="login">
+            <button className={classes.signoutBtn}>Sign out</button>
+          </NavLink>
+        </div>
+      </section>
     </>
   );
 }
