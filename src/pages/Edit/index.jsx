df05bf4064fe879af825ffaa7e48e6aaa33c6a56
Tatjana Oljaca <tatjanaoljaca@gmail.com>
2024-10-10 14:59:16 +0200

    functionality for edit profile page

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import avatar from './../../assets/icons/Avatar.svg';
 import backArrow from './../../assets/icons/BackArrow.svg';
 import classes from './EditProfile.module.css';
 import { NavLink, Form } from 'react-router-dom';
 
 export default function EditiProfilePage() {
+  const [userEditData, setUserEditData] = useState({
+    name: 'Tatjana',
+    email: '',
+    password: '',
+    weight: '',
+    height: '',
+    age: '',
+  });
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    console.log(userEditData);
+  };
+
+  const handleEditData = (e) => {
+    const { name, value } = e.target;
+    setUserEditData((prevData) => {
+      return { ...prevData, [name]: value };
+    });
+  };
+
   return (
     <>
       <header className={classes.editHeader}>
@@ -22,26 +44,26 @@ export default function EditiProfilePage() {
           <img src={avatar} alt="Avatar" />
         </div>
 
-        <Form className={classes.editForm}>
+        <Form className={classes.editForm} onSubmit={handleEditSubmit}>
           <div className={classes.inputFields}>
             <label>Full Name</label>
-            <input name="name" type="text" />
+            <input name="name" type="text" onChange={handleEditData} value={userEditData.name} />
           </div>
 
           <div className={classes.inputFields}>
             <label>Email address</label>
-            <input name="email" type="email" />
+            <input name="email" type="email" onChange={handleEditData} value={userEditData.email} />
           </div>
 
           <div className={classes.inputFields}>
             <label>Password</label>
-            <input name="password" type="text" />
+            <input name="password" type="text" onChange={handleEditData} value={userEditData.password} />
           </div>
 
           <div className={classes.inputFields}>
             <label>Weight</label>
             <div className={classes.weightContainer}>
-              <input name="weight" type="number" />
+              <input name="weight" type="number" onChange={handleEditData} value={userEditData.weight} />
               <span className={classes.unit}>kg</span>
             </div>
           </div>
@@ -49,7 +71,7 @@ export default function EditiProfilePage() {
           <div className={classes.inputFields}>
             <label>Height</label>
             <div className={classes.weightContainer}>
-              <input name="height" type="number" />
+              <input name="height" type="number" onChange={handleEditData} value={userEditData.height} />
               <span className={classes.unit}>cm</span>
             </div>
           </div>
@@ -58,6 +80,7 @@ export default function EditiProfilePage() {
             <label>Gender</label>
             <div className={classes.customSelect}>
               <select name="gender">
+                <option></option>
                 <option>Male</option>
                 <option>Female</option>
               </select>
@@ -66,7 +89,7 @@ export default function EditiProfilePage() {
 
           <div className={classes.inputFields}>
             <label>Age</label>
-            <input name="weight" type="number" />
+            <input name="age" type="number" onChange={handleEditData} value={userEditData.age} />
           </div>
 
           <button type="submit" className={classes.saveBtn}>
